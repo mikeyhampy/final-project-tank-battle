eda49128b4e2df27895d70eed4f9ef0cf67430a9
mikeyhampy <90983524+mikeyhampy@users.noreply.github.com>
2021-12-10 01:49:27 -0700

    fixed tank

--- tank-battle/game/tank.py
+++ tank-battle/game/tank.py
@@ -12,7 +12,7 @@ class Tank():
         tank_x1 = constants.TANK_X1
         position = Point(tank_x1, constants.TANK_Y)
         tank1.set_position(position)
-        tank1.set_width(constants.TANK_HEIGHT)
+        tank1.set_width(constants.TANK_WIDTH)
         tank1.set_height(constants.TANK_HEIGHT)
         tank1.set_image(constants.IMAGE_TANK1)
         self._tanks.append(tank1)
@@ -22,7 +22,7 @@ class Tank():
         tank_x2 = constants.TANK_X2
         position = Point(tank_x2, constants.TANK_Y)
         tank2.set_position(position)
-        tank2.set_width(constants.TANK_HEIGHT)
+        tank2.set_width(constants.TANK_WIDTH)
         tank2.set_height(constants.TANK_HEIGHT)
         tank2.set_image(constants.IMAGE_TANK2)
         self._tanks.append(tank2)
