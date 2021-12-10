6dffd1c2e4b92d2e2e29a5c7d5ede1ca48ea700d
mikeyhampy <90983524+mikeyhampy@users.noreply.github.com>
2021-12-10 11:38:00 -0700

    changed input service

--- tank-battle/game/input_service.py
+++ tank-battle/game/input_service.py
@@ -31,6 +31,19 @@ class InputService:
         dx = 0
         dy = 0
         fire = False
+        self.global_right = False
+ 
+
+        jump_right = False
+        if self.is_right_up():
+            self.global_right = True
+ 
+        if self.is_right_pressed() and self.global_right:
+            jump_right = True
+            self.global_right = False
+ 
+
+
 
         if self.is_left_pressed():
             dx = -1
@@ -98,41 +111,66 @@ class InputService:
         else:
             return False
 
+    # left pressed and not pressed
     def is_left_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_LEFT)
+    def is_left_up(self):
+        return raylibpy.is_key_up(raylibpy.KEY_LEFT)
 
+    # right pressed and not pressed
     def is_right_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_RIGHT)
+    def is_right_up(self):
+        return raylibpy.is_key_up(raylibpy.KEY_RIGHT)
 
+    # up pressed and not pressed
     def is_up_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_UP)
+    def is_up_up(self):
+        return raylibpy.is_key_up(raylibpy.KEY_UP)
 
+    # down pressed and not pressed
     def is_down_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_DOWN)
+    def is_down_up(self):
+        return raylibpy.is_key_up(raylibpy.KEY_DOWN)
 
+    # a pressed and not pressed
     def is_a_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_A)
-    
     def is_a_up(self):
         return raylibpy.is_key_up(raylibpy.KEY_A)
 
+    # d pressed and not pressed
     def is_d_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_D)
-
     def is_d_up(self):
         return raylibpy.is_key_up(raylibpy.KEY_D)
 
+    # w pressed and not pressed
     def is_w_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_W)
+    def is_w_up(self):
+        return raylibpy.is_key_up(raylibpy.KEY_W)
 
+    # s pressed and not pressed
     def is_s_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_S)
+    def is_s_up(self):
+        return raylibpy.is_key_up(raylibpy.KEY_S)
 
+    # space pressed and not pressed
     def is_space_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_SPACE)
-    
+    def is_space_up(self):
+        return raylibpy.is_key_up(raylibpy.KEY_SPACE)
+
+    # enter pressed and not pressed
     def is_enter_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_ENTER)
+    def is_enter_up(self):
+        return raylibpy.is_key_up(raylibpy.KEY_ENTER)
+
 
     def window_should_close(self):
         return raylibpy.window_should_close()
